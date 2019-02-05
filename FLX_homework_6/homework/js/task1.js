7d823e5a8bf0bb671e196c95a1dd29529ad3edbf
Roman Krychkovskyy <kruchkovskiy2@gmail.com>
2019-02-05 15:45:34 +0200

    added trim

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,11 +1,11 @@
-let value1 = prompt(`input a from quadratic equation ax2 + bx + c`, 0),
-    value2 = prompt(`input b from quadratic equation ${value1}x2 + bx + c`, 0),
-    value3 = prompt(`input c from quadratic equation ${value1}x2 + ${value2}x + c`, 0);
+let value1 = prompt(`input a from quadratic equation ax2 + bx + c`, 0).trim(),
+    value2 = prompt(`input b from quadratic equation ${value1}x2 + bx + c`, 0).trim(),
+    value3 = prompt(`input c from quadratic equation ${value1}x2 + ${value2}x + c`, 0).trim();
 if (value1 === null || value2 === null || value3 === null) {
     alert(`Invalid data`);
 } else if(isNaN(value1/2) || isNaN(value2/2) || isNaN(value3/2)){
     alert(`Invalid data`);
-} else if(value1 === 0 || value1 === '0') {
+} else if(value1 === 0 || value1 === '0' || value1.length === 0 || value2.length === 0 || value3.length === 0) {
     alert('Invalid data. a = 0 - it is not quadratic equation');
 } else {
     result(value1, value2, value3);
