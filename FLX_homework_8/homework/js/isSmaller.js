718935cfbae425487b798bb8fa60b0410be49295
Roman Krychkovskyy <kruchkovskiy2@gmail.com>
2019-02-09 19:10:28 +0200

    modify 8.2

--- FLX_homework_8/homework/js/isSmaller.js
+++ FLX_homework_8/homework/js/isSmaller.js
@@ -1,7 +1,10 @@
-function isSmaller(value1, value2) {
+function isBigger(value1, value2) {
     if (!Number(value1) || !Number(value2)) {
         return 'Incorrect data!';
     }
-    return value1 < value2;
+    return value1 > value2;
+}
+function isSmaller(value1, value2) {
+    return !isBigger(value1, value2);
 }
 isSmaller(5, -1);
