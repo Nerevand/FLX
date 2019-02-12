4d989dc186171a1e99028214d63317040b321648
Roman Krychkovskyy <kruchkovskiy2@gmail.com>
2019-02-12 16:30:45 +0200

    version №2

--- FLX_homework_8/homework/js/reverseNumber.js
+++ FLX_homework_8/homework/js/reverseNumber.js
@@ -2,6 +2,24 @@ function reverseNumber(x) {
     if (!Number.isInteger(x)) {
         return 'Incorrect data';
     }
-    return Math.sign(x) * Math.abs(x).toString().split('').reverse().join('');
+    //if we can't use reverse() below i add other answer on this type of task
+    // return Math.sign(x) * Math.abs(x).toString().split('').reverse().join('');
+    x = String(x).split('');
+    let negative = false;
+    let res = [];
+    let length = x.length;
+    if (x[0] === '-') {
+        negative = true;
+        x.shift();
+    }
+    for (let i = 0; i < length; i++) {
+        res.unshift(x.shift());
+    }
+    if (negative) {
+        res = res.join('') * (-1);
+    } else {
+        res = Number(res.join(''));
+    }
+    return res;
 }
 reverseNumber(123);
