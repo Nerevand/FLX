5546527e974501fa9d5ad8f9c79b29781e0cfbc3
Roman Krychkovskyy <kruchkovskiy2@gmail.com>
2019-02-18 15:54:33 +0200

    version №1

---
+++ FLX_homework_10/homework/src/credits_handler.js
@@ -0,0 +1,74 @@
+function userCard(value) {
+    let balance = 100;
+    let transactionLimit = 100;
+    let historyLogs = [];
+    let key = value;
+    let tax = 0.005;
+
+    let historyBox = (text, balance) => {
+        return historyLogs.push({
+            operationType: text,
+            credits: balance,
+            operationTime: new Date().toLocaleString('en-GB')
+        });
+    }
+    return {
+        getCardOptions: () => {
+            return {
+                key,
+                balance,
+                transactionLimit,
+                historyLogs
+            };
+        },
+        putCredits: credit => {
+            let text = `Received credits`;
+            balance += credit;
+            historyBox(text, balance);
+        },
+        takeCredits: function (credit) {
+            let text = `Withdrawal of credits`;
+            if (balance > credit && transactionLimit > credit) {
+                balance -= credit;
+                historyBox(text, balance);
+            } else {
+                console.error(`Sorry, but you can't do that. Your credit limit: ${transactionLimit}`);
+            }
+        },
+        setTransactionLimit: limit => {
+            let text = `Transaction limit change`;
+            transactionLimit = limit;
+            historyBox(text, limit);
+        },
+        transferCredits: function (value, card) {
+            let credits = value + tax * value;
+
+            if (balance >= credits && transactionLimit >= credits) {
+                this.takeCredits(credits);
+                card.putCredits(value);
+            } else {
+                console.error(`Sorry, but you can't do that. Your credit limit: ${transactionLimit}`);
+            }
+        }
+    };
+}
+
+class UserAccount {
+    constructor(name) {
+        this.name = name;
+        this.cards = [];
+        this.maxCards = 3;
+    }
+
+    addCard() {
+        if (this.maxCards > this.cards.length) {
+            this.cards.push(userCard(this.cards.length + 1));
+        } else {
+            console.error(`you have exceeded the limit`);
+        }
+    }
+
+    getCardByKey(key) {
+        return this.cards[key - 1];
+    }
+}
